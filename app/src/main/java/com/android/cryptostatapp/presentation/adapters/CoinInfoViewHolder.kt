9f4a28a7f18af25ad3ea1505700d881e05cac55b
mrMNK <zabuldigaa@gmail.com>
2022-07-04 20:20:14 +0300

    Use ViewBinding in adapter

---
+++ app/src/main/java/com/android/cryptostatapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.android.cryptostatapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.android.cryptostatapp.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+) : RecyclerView.ViewHolder(binding.root)
